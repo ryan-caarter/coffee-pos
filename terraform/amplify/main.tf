8e489b07ad757efcc2a6023fc0707477aaf2dbd9
ryancarter <ryan.c4rter@gmail.com>
2024-10-17 00:57:52 +1300

    auto-commit

--- terraform/amplify/main.tf
+++ terraform/amplify/main.tf
@@ -16,20 +16,20 @@ resource "aws_amplify_app" "main" {
   build_spec = <<-EOT
     version: 1
     frontend:
-    phases:
-        preBuild:
-        commands:
-            - npm ci --cache .npm --prefer-offline
-        build:
-        commands:
-            - npm run build
-    artifacts:
-        baseDirectory: build
-        files:
-        - '**/*'
-    cache:
-        paths:
-        - .npm/**/*
+        phases:
+            preBuild:
+                commands:
+                    - npm ci --cache .npm --prefer-offline
+            build:
+                commands:
+                    - npm run build
+        artifacts:
+            baseDirectory: build
+            files:
+                - '**/*'
+        cache:
+            paths:
+                - .npm/**/*
   EOT
 
  custom_rule {
@@ -39,6 +39,12 @@ resource "aws_amplify_app" "main" {
   }
   
   access_token = var.access_token
+
+  
+  environment_variables = {
+    WEBSOCKET_ENDPOINT = var.websocket_endpoint
+    STAGE = var.stage
+  }
 }
 
 resource "aws_amplify_branch" "main" {
