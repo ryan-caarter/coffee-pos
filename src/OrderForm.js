1204a6e45ab4736b3c4294976ac8796169583d8a
ryancarter <ryan.c4rter@gmail.com>
2024-11-24 21:43:24 +1300

    prettier

--- src/OrderForm.js
+++ src/OrderForm.js
@@ -12,8 +12,8 @@ const OrderForm = ({ addOrder }) => {
   const [extraNotes, setExtraNotes] = useState("");
   
   const handleSubmit = () => {
-    if (!selectedCoffee) {
-      alert("Bruh please select a coffee type");
+    if (!selectedCoffee && !pastry) {
+      alert("Bruh please select something to eat");
       return;
     }
 
@@ -42,7 +42,7 @@ const OrderForm = ({ addOrder }) => {
       <h2>Place an Order</h2>
       <h3>From the bakery</h3>
       {pastryOptions.map((pastry) => (
-        <button className="grid-item" style={{padding: 10, margin: 20}} key={pastry} onClick={() => setPastry(pastry)}>
+        <button className="grid-item" style={{padding: 10, margin: 10}} key={pastry} onClick={() => setPastry(pastry)}>
           {pastry}
         </button>
       ))}
@@ -52,14 +52,12 @@ const OrderForm = ({ addOrder }) => {
       <hr style={{ border: 'none', height: '5px', backgroundColor: 'rgb(74, 111, 74)' }}></hr>
       <h3>To drink</h3>
       {coffeeOptions.map((coffee) => (
-        <button className="grid-item" style={{padding: 10, margin: 20}} key={coffee} onClick={() => setSelectedCoffee(coffee)}>
+        <button className="grid-item" style={{padding: 10, margin: 10}} key={coffee} onClick={() => setSelectedCoffee(coffee)}>
           {coffee}
         </button>
       ))}
-      
-      
       {selectedCoffee && (
-        <div>
+        <>
             <h4>{selectedCoffee}</h4>
             <select className="option" name="milkType" onChange={(e) => setMilkType(e.target.value)}>
               <option value="Oat">Oat Milk</option>
@@ -67,30 +65,39 @@ const OrderForm = ({ addOrder }) => {
               <option value="Soy">Soy Milk</option>
               <option value="Regular">Regular Milk</option>
             </select>
-            <br></br>
-            <br></br>
-            <input
-            className="option"
-              type="text"
-              placeholder="Your name"
-              value={customerName}
-              onChange={(e) => setCustomerName(e.target.value)}
-            />
-            <br></br>
-            <br></br>
-            <input
-              className="option"
-              type="text"
-              placeholder="Extra notes, dietary requirements"
-              value={extraNotes}
-              onChange={(e) => setExtraNotes(e.target.value)}
-            />
-            <br></br>
-            <br></br>
-            <button className="option" onClick={handleSubmit}>Submit Order</button>
-
-        </div>
+        </>
       )}
+
+      {(pastry || selectedCoffee) && 
+      <>
+      <br></br>
+      <br></br>
+      <hr style={{ border: 'none', height: '5px', backgroundColor: 'rgb(74, 111, 74)' }}></hr> 
+
+      <br></br>
+      <input
+      className="option"
+        type="text"
+        placeholder="Your name"
+        value={customerName}
+        onChange={(e) => setCustomerName(e.target.value)}
+      />
+      <br></br>
+      <br></br>
+      <input
+        className="option"
+        type="text"
+        placeholder="Extra notes, dietary requirements"
+        value={extraNotes}
+        onChange={(e) => setExtraNotes(e.target.value)}
+      />
+      <br></br>
+      <br></br>
+      <button className="option" onClick={handleSubmit}>Submit Order</button>
+      <br></br>
+      <br></br>
+      </>
+    }
     </div>
   );
 };
